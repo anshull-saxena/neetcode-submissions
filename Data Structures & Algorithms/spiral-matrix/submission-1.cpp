a0ff7b1b8317dad412262e390ccad3ea90c40415
Anshul Saxena <119007693+anshull-saxena@users.noreply.github.com>
2026-09-06 01:07:57 +0530

    Add: spiral-matrix - submission-1

---
+++ Data Structures & Algorithms/spiral-matrix/submission-1.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int top = 0, bottom = matrix.size() - 1;
+        int left = 0, right = matrix[0].size() - 1;
+        vector<int> ans;
+
+        while (top <= bottom && left <= right) {
+            // top row
+            for (int i = left; i <= right; ++i)
+                ans.push_back(matrix[top][i]);
+            ++top;
+
+            // right column
+            for (int i = top; i <= bottom; ++i)
+                ans.push_back(matrix[i][right]);
+            --right;
+
+            if (top <= bottom) { // bottom row
+                for (int i = right; i >= left; --i)
+                    ans.push_back(matrix[bottom][i]);
+                --bottom;
+            }
+
+            if (left <= right) { // left column
+                for (int i = bottom; i >= top; --i)
+                    ans.push_back(matrix[i][left]);
+                ++left;
+            }
+        }
+        return ans;
+    }
+};
